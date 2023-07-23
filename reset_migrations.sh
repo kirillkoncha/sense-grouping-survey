3c8dfdd50011d219cb24d22c468d63bcfd3b0993
Kirill Koncha <42929201+kirillkoncha@users.noreply.github.com>
2023-07-23 14:07:15 +0300

    Update reset_migrations.sh

--- reset_migrations.sh
+++ reset_migrations.sh
@@ -3,4 +3,4 @@ rm db.sqlite3
 ./manage.py makemigrations
 ./manage.py migrate
 ./manage.py loaddata survey/fixtures/initial_data.yaml
-./manage.py loaddata survey/fixtures/contexts.yaml
+./manage.py loaddata survey/fixtures/contexts_exp.yaml
